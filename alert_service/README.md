c0d5a119b60464dadbcf72c8a29bd5fdf06035c1
Tomdieu <ivantomdio@gmail.com>
2023-12-29 17:06:26 +0100

    feat : added alert service

---
+++ alert_service/README.md
@@ -0,0 +1,21 @@
+# Alert Service
+
+## Description
+
+This service is responsible for sending alerts to the user.
+
+## Functionality
+
+- System should provide real-time alerts for different types of disasters.
+
+# Installation
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running the app
+
+```bash
+python manage.py runserver
+```
---
+++ alert_service/alerts/models.py
@@ -0,0 +1,142 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Location(models.Model):
+    lat = models.FloatField(default=0.0, null=False, blank=False)
+    lng = models.FloatField(default=0.0, null=False, blank=False)
+    zip_code = models.CharField(max_length=255, null=True, blank=True)
+    locality = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.lat} - {self.lng}"
+
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    profile_image = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    # SPECIALIZATION_CHOICES = [
+    #     ("Firefighting", "Firefighting"),
+    #     ("Law Enforcement", "Law Enforcement"),
+    #     ("Emergency Medical Services (EMS)", "Emergency Medical Services (EMS)"),
+    #     ("Search and Rescue", "Search and Rescue"),
+    #     (
+    #         "Hazardous Materials (HazMat) Response",
+    #         "Hazardous Materials (HazMat) Response",
+    #     ),
+    #     ("Technical Rescue", "Technical Rescue"),
+    #     ("Urban Search and Rescue (USAR)", "Urban Search and Rescue (USAR)"),
+    #     ("Critical Incident Response", "Critical Incident Response"),
+    #     ("Medical Specialization", "Medical Specialization"),
+    #     ("Disaster Response and Recovery", "Disaster Response and Recovery"),
+    #     ("Communications and Coordination", "Communications and Coordination"),
+    #     ("Aviation Rescue", "Aviation Rescue"),
+    # ]
+
+    def __str__(self):
+        return f"{self.username} - {self.type}"
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    skills = models.CharField(max_length=255)
+    interests = models.CharField(max_length=255)
+
+    is_online = models.BooleanField(default=False)
+    last_activity = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} - {self.skills}"
+
+
+class UserReport(models.Model):
+    IMPACT = [
+        ("low", "Low"),
+        ("moderate", "Moderate"),
+        ("high", "High"),
+    ]
+
+    URGENCY = [
+        ("low", "Low"),
+        ("moderate", "Moderate"),
+        ("high", "High"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    impact = models.CharField(max_length=255, choices=IMPACT)
+    urgency = models.CharField(max_length=255, choices=URGENCY)
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.user.username}"
+
+
+class Alert(models.Model):
+    TYPES = {
+        "Natural Disasters": {
+            "earthquakes": "Earthquakes",
+            "floods": "Floods",
+            "hurricanes/cyclones/typhoons": "Hurricanes/Cyclones/Typhoons",
+            "tornadoes": "Tornadoes",
+            "tsunamis": "Tsunamis",
+            "wildfires": "Wildfires",
+            "volcanic eruptions": "Volcanic eruptions",
+            "fire": "Fire",
+        },
+        "Man-Made Disasters": {
+            "industrial accidents": "Industrial Accidents",
+            "nuclear incidents": "Nuclear Incidents",
+            "hazardous material spills": "Hazardous Material Spills",
+            "transportation accidents": "Transportation Accidents (e.g., plane crashes)",
+            "structural failures": "Structural Failures (e.g., building collapses)",
+        },
+        "Public Health Emergencies": {
+            "pandemics": "Pandemics",
+            "disease outbreaks": "Disease Outbreaks",
+            "biological threats": "Biological Threats",
+        },
+        "Weather-Related Events": {
+            "extreme temperature events": "Extreme Temperature Events (Heatwaves, Cold Snaps)",
+            "severe storms": "Severe Storms (Heavy Rainfall, Thunderstorms)",
+            "snowstorms/blizzards": "Snowstorms/Blizzards",
+        },
+    }
+
+    SEVERITY = [
+        ("low", "Low"),
+        ("moderate", "Moderate"),
+        ("high", "High"),
+        ("critical", "Critical"),
+    ]
+
+    AUDIENCE_CHOICES = [
+        ("public", "Public"),
+        ("emergency_responders", "Emergency Responders"),
+    ]
+
+    title = models.CharField(max_length=255)
+
+    description = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=TYPES)
+    severity = models.CharField(max_length=100, choices=SEVERITY)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    audience = models.CharField(max_length=20, choices=AUDIENCE_CHOICES)
+
+    def __str__(self):
+        return f"{self.title} - {self.type}"

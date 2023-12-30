fb7017cd5c9a106b05831755ec8bfe92aaaf3e16
Tomdieu <ivantomdio@gmail.com>
2023-12-30 20:56:15 +0100

    feat : updated visitor

--- user_service/accounts/patterns/visitor.py
+++ user_service/accounts/patterns/visitor.py
@@ -1,19 +1,24 @@
-from accounts.models import Citizen, EmergencyResponder
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from accounts.models import Citizen, EmergencyResponder
+else:
+    Citizen = EmergencyResponder = None
 
 
 class Visitor:
 
-    def visit_citizen(self, citizen: Citizen):
+    def visit_citizen(self, citizen: 'Citizen'):
         pass
 
-    def visit_emergency_responder(self, emergency_responder: EmergencyResponder):
+    def visit_emergency_responder(self, emergency_responder: 'EmergencyResponder'):
         pass
 
 
 class NotifyVisitor(Visitor):
 
-    def visit_citizen(self, citizen: Citizen):
+    def visit_citizen(self, citizen: 'Citizen'):
         pass
 
-    def visit_emergency_responder(self, emergency_responder: EmergencyResponder):
+    def visit_emergency_responder(self, emergency_responder: 'EmergencyResponder'):
         pass

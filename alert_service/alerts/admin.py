d0222accb16a3569abe7420b35506b288c814b7d
Tomdieu <ivantomdio@gmail.com>
2023-12-30 21:13:55 +0100

    feat : updated admin

--- alert_service/alerts/admin.py
+++ alert_service/alerts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Alert, Location, Profile, User, UserReport
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'lat', 'lng', 'zip_code', 'locality']
+    list_filter = ['zip_code', 'locality']
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'user', 'title', 'description', 'location', 'created_at', 'updated_at']
+    list_filter = ['user', 'location', 'created_at', 'updated_at']
+
+@admin.register(UserReport)
+class UserReportAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'user', 'title', 'description', 'location', 'impact', 'urgency', 'created_at', 'updated_at']
+    list_filter = ['user', 'location', 'impact', 'urgency', 'created_at', 'updated_at']
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ['username','email','phone_number','type','gender']
+    list_filter = ['username','email','type','gender']
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = ['user','location','skills','interests','is_online','last_activity']
+    list_filter = ['user','location','skills','interests','is_online','last_activity']

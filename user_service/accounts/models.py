4738e19503ccc16dbaae021f5460110d573b5df4
Tomdieu <ivantomdio@gmail.com>
2023-12-31 02:20:12 +0100

    feat updated models

--- user_service/accounts/models.py
+++ user_service/accounts/models.py
@@ -19,6 +19,7 @@ class Location(models.Model):
     def __str__(self):
         return f"{self.lat} - {self.lng}"
 
+
 class User(AbstractUser):
     GENDER_CHOICES = [
         ("M", "Male"),
@@ -94,27 +95,27 @@ class Citizen(User):
     def accept(self, visitor: Visitor):
         visitor.visit_citizen(self)
 
+
 SPECIALIZATION_CHOICES = [
-        ("Firefighting", "Firefighting"),
-        ("Law Enforcement", "Law Enforcement"),
-        ("Emergency Medical Services (EMS)", "Emergency Medical Services (EMS)"),
-        ("Search and Rescue", "Search and Rescue"),
-        (
-            "Hazardous Materials (HazMat) Response",
-            "Hazardous Materials (HazMat) Response",
-        ),
-        ("Technical Rescue", "Technical Rescue"),
-        ("Urban Search and Rescue (USAR)", "Urban Search and Rescue (USAR)"),
-        ("Critical Incident Response", "Critical Incident Response"),
-        ("Medical Specialization", "Medical Specialization"),
-        ("Disaster Response and Recovery", "Disaster Response and Recovery"),
-        ("Communications and Coordination", "Communications and Coordination"),
-        ("Aviation Rescue", "Aviation Rescue"),
-    ]
+    ("Firefighting", "Firefighting"),
+    ("Law Enforcement", "Law Enforcement"),
+    ("Emergency Medical Services (EMS)", "Emergency Medical Services (EMS)"),
+    ("Search and Rescue", "Search and Rescue"),
+    (
+        "Hazardous Materials (HazMat) Response",
+        "Hazardous Materials (HazMat) Response",
+    ),
+    ("Technical Rescue", "Technical Rescue"),
+    ("Urban Search and Rescue (USAR)", "Urban Search and Rescue (USAR)"),
+    ("Critical Incident Response", "Critical Incident Response"),
+    ("Medical Specialization", "Medical Specialization"),
+    ("Disaster Response and Recovery", "Disaster Response and Recovery"),
+    ("Communications and Coordination", "Communications and Coordination"),
+    ("Aviation Rescue", "Aviation Rescue"),
+]
+
 
 class EmergencyResponder(User):
-
-
     emergency_contact_number = models.CharField(max_length=20, blank=True, null=True)
     emergency_contact_person = models.CharField(max_length=255, blank=True, null=True)
     is_verified = models.BooleanField(default=False)
@@ -136,7 +137,6 @@ class EmergencyResponder(User):
 
 
 class EmergencyResponseTeam(models.Model):
-
     team_name = models.CharField(max_length=50)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="team_location")
     members = models.ManyToManyField(EmergencyResponder)

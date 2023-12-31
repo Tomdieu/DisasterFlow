68a9765db32ff198cd15577f4e1e13f9bf8c2f76
Tomdieu <ivantomdio@gmail.com>
2024-01-01 00:08:43 +0100

    feat : remove locations from api path

--- alert_service/alerts/api/urls.py
+++ alert_service/alerts/api/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import LocationViewSet,UserReportViewSet,AlertViewSet
 
 router = DefaultRouter()
-router.register(r'locations',LocationViewSet)
+# router.register(r'locations',LocationViewSet)
+
 router.register(r'user-reports',UserReportViewSet)
 router.register(r'alerts',AlertViewSet)
 

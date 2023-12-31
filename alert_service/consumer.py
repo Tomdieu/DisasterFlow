ae1b7d7b2a3c25566601da6c07cbc49d844465c7
Tomdieu <ivantomdio@gmail.com>
2023-12-31 14:21:02 +0100

    feat : added consumer

---
+++ alert_service/consumer.py
@@ -0,0 +1,26 @@
+import pika,os,json
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alert_service.settings")
+django.setup()
+
+from django.conf import settings
+
+from alerts.models import User,Profile
+
+credentials = pika.PlainCredentials(settings.RABBITMQ_USERNAME, settings.RABBITMQ_PASSWORD)
+parameters = pika.ConnectionParameters(settings.RABBITMQ_HOST, settings.RABBITMQ_PORT, settings.RABBITMQ_VHOST, credentials)
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='alert')
+
+
+def callback(ch,method,properties,body):
+
+    print("New Message Recieve")
+
+channel.basic_consume(queue='alert',on_message_callback=callback,auto_ack=True)
+
+channel.start_consuming()

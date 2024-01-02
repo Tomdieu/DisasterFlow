922c16b8b729eb05231ab1e702e758d336b9266b
Tomdieu <ivantomdio@gmail.com>
2024-01-02 23:39:08 +0100

    switch from sqlite3 engline to spatialite

--- emergency_responder_service/emergency_responder_service/settings.py
+++ emergency_responder_service/emergency_responder_service/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'emergency_responder_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        "ENGINE": "django.contrib.gis.db.backends.spatialite",
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }

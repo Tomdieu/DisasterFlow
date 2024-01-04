ccbce8a776d106c727af2c2a1f344f529941163c
Tomdieu <ivantomdio@gmail.com>
2024-01-04 01:11:10 +0100

    feat : added events app

---
+++ emergency_responder_service/core/events.py
@@ -0,0 +1,45 @@
+USER_CREATED = "user_created"
+USER_UPDATED = "user_updated"
+USER_DELETED = "user_deleted"
+
+PROFILE_UPDATED = "profile_updated"
+PROFILE_CREATED = "profile_created"
+
+EMERGENCY_RESPONDER_CREATED = "emergency_responder_created"
+EMERGENCY_RESPONDER_UPDATED = "emergency_responder_updated"
+EMERGENCY_RESPONDER_DELETED = "emergency_responder_deleted"
+
+CITIZEN_CREATED = "citizen_created"
+CITIZEN_UPDATED = "citizen_updated"
+CITIZEN_DELETED = "citizen_deleted"
+
+LOCATION_CREATED = "location_created"
+LOCATION_UPDATED = "location_updated"
+
+
+ALERT_CREATED = "alert_created"
+ALERT_UPDATED = "alert_updated"
+
+EMERGENCY_ACTION_CREATED = "emergency_action_created"
+EMERGENCY_ACTION_UPDATED = "emergency_action_updated"
+
+EMERGENCY_RESPONSE_TEAM_CREATED = "emergency_response_team_created"
+EMERGENCY_RESPONSE_TEAM_UPDATED = "emergency_response_team_updated"
+
+RESOURCE_CREATED = "resource_created"
+RESOURCE_UPDATED = "resource_updated"
+
+MESSAGE_CREATED = "message_created"
+MESSAGE_UPDATED = "message_updated"
+
+EMERGENCY_RESPONDER_ADDED_TO_TEAM = "emergency_responder_added_to_team"
+EMERGENCY_RESPONDER_REMOVED_FROM_TEAM = "emergency_responder_removed_from_team"
+
+EMERGENCY_RESPONDER_ONLINE = "emergency_responder_online"
+EMERGENCY_RESPONDER_OFFLINE = "emergency_responder_offline"
+
+EMERGENCY_RESPONDER_VERIFIED = "emergency_responder_verified"
+EMERGENCY_RESPONDER_UNVERIFIED = "emergency_responder_unverified"
+
+EMERGENCY_RESPONSE_TEAM_VERIFIED = "emergency_response_team_verified"
+EMERGENCY_RESPONSE_TEAM_UNVERIFIED = "emergency_response_team_unverified"
